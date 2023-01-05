38ec65b18e185908cdd8a138275a4518097fd4c1
Gonzalo Nuñez <gonzaloblasnunez@gmail.com>
2023-01-05 13:07:15 -0500

    More WIP

--- pages/reading/index.js
+++ pages/reading/index.js
@@ -4,14 +4,27 @@ export default function Reading() {
   return (
     <Article title='Reading'>
       <p>
-        Below is a list of the very best books, articles, etc. I've read either
-        in print or on the internet.
+        The very best authors, books, essays and/or collected works that I have
+        read either in print or on the internet. You can find my Substack
+        recommendations over at{' '}
+        <a href='https://gonzalonunez.substack.com' target='_blank'>
+          A Work in Progress
+        </a>
+        .
       </p>
-      <h4>Books</h4>
+      <h4>Philosophy / Politics</h4>
       <ul>
         <li>Hello</li>
       </ul>
-      <h4>Articles</h4>
+      <h4>Fiction</h4>
+      <ul>
+        <li>Hello</li>
+      </ul>
+      <h4>Non-fiction</h4>
+      <ul>
+        <li>Hello</li>
+      </ul>
+      <h4>Essays</h4>
       <ul>
         <li>Hello</li>
       </ul>
