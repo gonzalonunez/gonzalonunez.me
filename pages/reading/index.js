35272a6c5384dbbf91a90ae92470272631b6dcb7
Gonzalo Nuñez <gonzaloblasnunez@gmail.com>
2023-01-06 12:06:35 -0500

    WIP

--- pages/reading/index.js
+++ pages/reading/index.js
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import Article from '../../components/Article';
 
 function ReadingItem(props) {
   return (
     <li>
-      <div className=''>{props.item.title}</div>
-      <div className='text-xs text-neutral-400'>{props.item.subtitle}</div>
+      <a className='select-none' href='https://amazon.com' target='_blank'>
+        {props.item.title}
+      </a>
+      <div className='prose text-xs text-neutral-400 mt-1'>
+        {props.item.subtitle && `${props.item.subtitle}`}
+        {props.item.subtitle && props.item.details && ` · `}
+        {props.item.details}
+      </div>
     </li>
   );
 }
@@ -12,7 +19,7 @@ function ReadingItem(props) {
 function ReadingItems(props) {
   return (
     <div>
-      <h4>{props.title}</h4>
+      <h4 className='select-none'>{props.title}</h4>
       <ul>
         {props.items.map(item => (
           <ReadingItem item={item} />
@@ -26,19 +33,27 @@ export default function Reading() {
   const content = {
     philosophy: [
       {
-        title: 'Nietzsche (Everything)'
+        title: 'Nietzsche (Everything)',
+        details:
+          'Ignoring Nietzsche is a mistake. Disregard popular misconceptions about him while also discounting his later work. “Each of us can discover a different Nietzsche to admire and/or detest.” — Shelley Frisch.'
       },
       {
         title: 'Nietzsche: A Philosophical Biography',
-        subtitle: 'Rüdiger Safranski'
+        subtitle: 'Rüdiger Safranski',
+        details:
+          "Highly recommend. Extremely easy to read and covers all of Nietzsche's work in the context of his life. A fantastic supplement to the primary sources."
       },
       {
         title: 'The Human Condition',
-        subtitle: 'Hannah Arendt'
+        subtitle: 'Hannah Arendt',
+        details:
+          'Prescient book, written in 1958 but diagnoses a lot of "modern" problems. Characteristically Arendtian, but easier to contextualize than some of her other work. Helpful introduction to Ancient Greek philosophy.'
       },
       {
         title: 'The Machiavellians',
-        subtitle: 'James Burnham'
+        subtitle: 'James Burnham',
+        details:
+          'A book you cannot unsee, changes the way you view politics forever.'
       },
       {
         title: 'Essays and Aphorisms',
@@ -46,33 +61,47 @@ export default function Reading() {
       },
       {
         title: 'In the Presence of Schopenhauer',
-        subtitle: 'Michel Houellebecq'
+        subtitle: 'Michel Houellebecq',
+        details:
+          "In Schopenhauer, Houellebecq found a companion. This short book shines a light on Houellebecq's worldview. Crucial if you want to understand Houellebecq."
       },
       {
         title: 'The End of History',
-        subtitle: 'Francis Fukuyama'
+        subtitle: 'Francis Fukuyama',
+        details:
+          'Fukuyama was right. Strong and widely misunderstood thesis, more relevant with each passing day. Written for the modern reader, weaves a beautiful tapestry of history and philosophy. Also a great introduction to Hegel and supplement to Nietzsche.'
       }
     ],
     fiction: [
       {
         title: 'The Elementary Particles',
-        subtitle: 'Michel Houellebecq'
+        subtitle: 'Michel Houellebecq',
+        details:
+          'My favorite from Houellebecq thus far. Warning: graphic and gruesome, but another instance of an author presciently diagnosing some of our modern ills.'
       },
       {
         title: 'The Map and The Territory',
-        subtitle: 'Michel Houellebecq'
+        subtitle: 'Michel Houellebecq',
+        details:
+          "The most palatable of Houellebecq's novels. Good introduction to Houellebecq, but not his best for probably the same reasons."
       },
       {
-        title: 'The Complete Short Stories (Finca Vigía)',
-        subtitle: 'Ernest Hemingway'
+        title: 'The Complete Short Stories',
+        subtitle: 'Ernest Hemingway',
+        details:
+          'Hemingway at his best. Memorable stories include "The Snows of Kilimanjaro", "Che Te Dice La Patria?", "Today is Friday", and "The Short Happy Life of Francis Macomber".'
       },
       {
         title: 'The Sun Also Rises',
-        subtitle: 'Ernest Hemingway'
+        subtitle: 'Ernest Hemingway',
+        details:
+          'Simple writing that reads as if you were watching a movie in your head. Good gateway drug if you want to get back into fiction.'
       },
       {
         title: 'The Brothers Karamazov',
-        subtitle: 'Dostoevsky'
+        subtitle: 'Dostoevsky',
+        details:
+          'Once Nietzsche make you mean, Dostoevsky will save you. Long and complex, a book that calls to be reread over the course of a lifetime.'
       },
       {
         title: 'Zen and the Art of Motorcycle Maintenance',
@@ -82,11 +111,15 @@ export default function Reading() {
     nonfiction: [
       {
         title: 'Cuba',
-        subtitle: 'Erna Fergusson'
+        subtitle: 'Erna Fergusson',
+        details:
+          'Best book on Cuba that I have ever read. Shines a light on 1940s pre-revolutionary Cuba from a first-person perspective. The author sits down and has a coffee with my grandfather.'
       },
       {
         title: 'The Sugar King of Havana',
-        subtitle: 'John Paul Rathbone'
+        subtitle: 'John Paul Rathbone',
+        details:
+          "Another great book on Cuba. It is every Cuban-American's dream to tell the story of the Revolution while weaving in some family history. Great insight on Julio Lobo, very much a live-player in all of the Cuban drama."
       },
       {
         title: 'Leonardo da Vinci',
@@ -94,15 +127,21 @@ export default function Reading() {
       },
       {
         title: 'Bolívar',
-        subtitle: 'Marie Arana'
+        subtitle: 'Marie Arana',
+        details:
+          'Fun read, does not shy away from the complex details of such a monumental figure. Doubles as a lesson in Latin American history.'
       },
       {
         title: "Don't Tell Me I Can't",
-        subtitle: 'Cole Summers'
+        subtitle: 'Cole Summers',
+        details:
+          "You'll read it in an hour and will never forget it. Raw, concentrated optimism and hope in a better future from the mind of a 14 year-old."
       },
       {
         title: 'The Timeless Way of Building',
-        subtitle: 'Christopher Alexander'
+        subtitle: 'Christopher Alexander',
+        details:
+          'Contains the secrets of the universe. A wonderful introduction into the world of a seminal and perenially underrated thinker.'
       },
       {
         title: 'The War of Art',
@@ -116,7 +155,9 @@ export default function Reading() {
     essays: [
       {
         title: 'The White Album',
-        subtitle: 'Joan Didion'
+        subtitle: 'Joan Didion',
+        details:
+          'I try to always have easy access to something by Didion. Makes you want to write. Not for everybody, but a staunch favorite of those for whom it is.'
       },
       {
         title: 'Slouching Towards Bethlehem',
@@ -140,7 +181,9 @@ export default function Reading() {
       },
       {
         title: 'Adult Fantasy of Youth',
-        subtitle: 'Marilyn Simon'
+        subtitle: 'Marilyn Simon',
+        details:
+          'Motivated me to watch the film, which is now one of my favorites. Beautiful and delicate analysis of a complex film.'
       },
       {
         title: 'How an 18th Century Philosopher Helped Solve My Midlife Crisis',
@@ -148,7 +191,9 @@ export default function Reading() {
       },
       {
         title: 'La Patria a flor de piel',
-        subtitle: 'IMarie Nuñez'
+        subtitle: 'IMarie Nuñez',
+        details:
+          'A piece of family history written by my aunt, from the perspective of my grandmother.'
       },
       {
         title: 'This is Water',
@@ -156,7 +201,9 @@ export default function Reading() {
       },
       {
         title: 'From Mises to Carlyle',
-        subtitle: 'Mencius Moldbug'
+        subtitle: 'Mencius Moldbug',
+        details:
+          "Critical to understanding Moldbug, the closest person we have today to a philosopher-king. Strong diagnosis of some of today's political failures. Just like Nietzsche, ignoring Moldbug is a mistake."
       },
       {
         title: 'A World Split Apart',
@@ -164,13 +211,17 @@ export default function Reading() {
       },
       {
         title: 'The End of History (Essay)',
-        subtitle: 'Francis Fukuyama'
+        subtitle: 'Francis Fukuyama',
+        details:
+          'Worth reading before diving into the book, which takes the essay and then proceeds to blow it out of the water.'
       }
     ],
     poetry: [
       {
         title: 'Burning in Water, Drowning in Flame',
-        subtitle: 'Charles Bukowski'
+        subtitle: 'Charles Bukowski',
+        details:
+          'Turns out you can just buy a book of poems and read it front-to-back as if it were a novel.'
       }
     ]
   };
