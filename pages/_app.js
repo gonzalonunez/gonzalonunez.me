52403b394abebe519a9270011a225e9eba8dd8ef
Gonzalo Nuñez <gonzaloblasnunez@gmail.com>
2023-02-10 17:33:50 -0800

    Add favico

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,33 @@ export default function MyApp({ Component, pageProps }) {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <NavigationContext.Provider value={{ isOpen, setIsOpen }}>
-      <Head></Head>
+      <Head>
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/static/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/static/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/static/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/static/site.webmanifest' />
+        <link
+          rel='mask-icon'
+          href='/static/safari-pinned-tab.svg'
+          color='#5bbad5'
+        />
+        <meta name='msapplication-TileColor' content='#da532c' />
+        <meta name='theme-color' content='#ffffff' />
+      </Head>
       <SiteLayout>
         <Component {...pageProps} />
       </SiteLayout>
