a346c89cb5c1440b8a93f2b712432ef65cfbe114
Gonzalo Nuñez <gonzaloblasnunez@gmail.com>
2023-02-21 22:56:45 -0500

    Small tweaks

--- components/Article.js
+++ components/Article.js
@@ -1,7 +1,7 @@
 export default function Article(props) {
   return (
     <div className='flex justify-center px-lg'>
-      <article className='prose prose-neutral w-full underline-offset-4'>
+      <article className='prose prose-neutral w-full underline-offset-4 select-none'>
         {props.title && <h1 className='font-title'>{props.title}</h1>}
         {props.children}
       </article>
