93d31c39a197354c9c237b7b320ab2112e1877c9
Gonzalo Nuñez <gonzaloblasnunez@gmail.com>
2023-02-21 22:58:30 -0500

    Tweak

--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,7 @@ export default function About() {
     <Article title='About'>
       <p>
         <b>Hi, I'm Gonzalo.</b> I think about art, culture, startups, and
-        philosophy. I'm currently living in San Francisco, California.
+        philosophy and I'm currently living in San Francisco, California.
       </p>
       <ListItems title='Work' items={work} />
       <ListItems title='Essays' items={essays} />
