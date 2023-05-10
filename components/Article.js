715f3679903ee7514721a8ee350305c40eaaf716
Gonzalo Nuñez <gonzaloblasnunez@gmail.com>
2023-05-09 20:33:08 -0400

    Add bottom padding

--- components/Article.js
+++ components/Article.js
@@ -1,6 +1,6 @@
 export default function Article(props) {
   return (
-    <div className='flex justify-center px-lg'>
+    <div className='flex justify-center px-lg pb-6'>
       <article className='prose prose-neutral w-full underline-offset-4 select-none'>
         {props.title && <h1 className='font-title mb-0'>{props.title}</h1>}
         {props.children}
